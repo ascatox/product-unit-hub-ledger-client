fd1711fb42f40fd1afa5834a5647c3f370a42275
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2018-02-28 14:12:19 +0100

    Initial commit.

---
+++ src/main/java/it/eng/productunithubledgerclient/ProductUnitHubLedgerClientApplication.java
@@ -0,0 +1,12 @@
+package it.eng.productunithubledgerclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductUnitHubLedgerClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductUnitHubLedgerClientApplication.class, args);
+	}
+}
---
+++ src/test/java/it/eng/productunithubledgerclient/ProductUnitHubLedgerClientApplicationTests.java
@@ -0,0 +1,16 @@
+package it.eng.productunithubledgerclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductUnitHubLedgerClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
